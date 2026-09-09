3cd88f650d683639616735829f896c45614677a8
Javi <javipiagui13@gmail.com>
2026-09-09 10:26:28 +0200

    feat: remove unused assets

--- src/components/ai-detector/CtaSection.tsx
+++ src/components/ai-detector/CtaSection.tsx
@@ -1,13 +1,10 @@
-import React from 'react';
-import styles from '../../components/techradar-discount/Background.module.scss';
-
 const CtaSection = ({ textContent }) => {
   return (
     <section className="overflow-hidden py-12">
       <div className="flex flex-col items-center justify-center pt-12 lg:px-20">
         <div className="flex flex-col text-center text-white">
           <p className="text-4xl font-semibold">{textContent.title}</p>
-          <p className="pt-4 pb-5 text-xl font-normal">{textContent.description}</p>
+          <p className="pb-5 pt-4 text-xl font-normal">{textContent.description}</p>
         </div>
         <button
           onClick={() => {
@@ -19,7 +16,7 @@ const CtaSection = ({ textContent }) => {
         </button>
       </div>
       <div
-        className={`absolute top-16 left-0 -z-10 flex h-screen w-screen ${styles.partnerHeroSection} pointer-events-none origin-center`}
+        className={`pointer-events-none absolute left-0 top-16 -z-10 flex h-screen w-screen origin-center bg-primary`}
       />
     </section>
   );
--- src/components/annual-plans-for-affiliates/components/PriceTable.tsx
+++ src/components/annual-plans-for-affiliates/components/PriceTable.tsx
@@ -3,9 +3,8 @@ import { Transition } from '@headlessui/react';
 import CardSkeleton from '@/components/components/CardSkeleton';
 
 import PriceCard from './PriceCard';
-import { CurrencyCircleDollar, Lifebuoy } from '@phosphor-icons/react';
+import { CurrencyCircleDollar, Lifebuoy, Keyhole } from '@phosphor-icons/react';
 
-import OpenSource from '../../../../public/icons/open-source.svg';
 import usePricing from '@/hooks/usePricing';
 import { PromoCodeName } from '@/lib/types';
 
@@ -42,7 +41,7 @@ const PriceTable: React.FC<PriceTableProps> = ({
       text: textContent.features.guarantee,
     },
     {
-      icon: OpenSource,
+      icon: Keyhole,
       text: textContent.features.openSource,
     },
   ];
